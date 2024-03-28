b70e8fda8ae9af9b3afa59e8f8b91c2ae436c259
Max Howell <mxcl@me.com>
2024-03-28 13:11:43 -0400

    Initial Commit

---
+++ pkgx/pkgxApp.swift
@@ -0,0 +1,17 @@
+//
+//  pkgxApp.swift
+//  pkgx
+//
+//  Created by Maxim Howell on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct pkgxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
