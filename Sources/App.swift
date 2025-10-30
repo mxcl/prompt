b4bce20a63eb33fe29c1c59720178be1c713dfaa
Max Howell <mxcl@me.com>
2025-10-30 11:34:33 -0400

    Convert app to menu bar status item

--- Sources/App.swift
+++ Sources/App.swift
@@ -3,9 +3,10 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
     var windowController: MainWindowController?
     private var settingsWindowController: SettingsWindowController?
+    private var statusItem: NSStatusItem?
 
     func applicationDidFinishLaunching(_ notification: Notification) {
-        setupMenu()
+        setupStatusItem()
         setupApplication()
         setupGlobalShortcut()
         // Initialize cask data loading
@@ -13,10 +14,9 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
     private func setupApplication() {
-        NSApp.setActivationPolicy(.regular)
+        NSApp.setActivationPolicy(.accessory)
 
         windowController = MainWindowController()
-        windowController?.showWindow(self)
     }
 
     private func setupGlobalShortcut() {
@@ -26,109 +26,62 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
     @objc private func toggleWindow() {
+        guard let window = windowController?.window else {
+            showMainWindow()
+            return
+        }
+
+        if window.isVisible && NSApp.isActive && window.isKeyWindow {
+            hideMainWindow()
+        } else {
+            showMainWindow()
+        }
+    }
+
+    private func setupStatusItem() {
+        let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+        self.statusItem = statusItem
+
+        if let button = statusItem.button {
+            if let image = NSImage(systemSymbolName: "cup.and.saucer.fill", accessibilityDescription: "teaBASE") {
+                image.isTemplate = true
+                button.image = image
+            } else {
+                button.title = "tea"
+            }
+        }
+
+        let menu = NSMenu()
+
+        let openItem = NSMenuItem(title: "Open Window", action: #selector(openWindowFromStatusItem), keyEquivalent: "")
+        openItem.target = self
+        menu.addItem(openItem)
+
+        menu.addItem(NSMenuItem.separator())
+
+        let quitItem = NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q")
+        quitItem.target = NSApp
+        menu.addItem(quitItem)
+
+        statusItem.menu = menu
+    }
+
+    @objc private func openWindowFromStatusItem() {
+        showMainWindow()
+    }
+
+    private func showMainWindow() {
         guard let windowController = windowController else {
             return
         }
 
-        if let window = windowController.window {
-            if window.isVisible && NSApp.isActive && window.isKeyWindow {
-                // Window is visible, app is active, and window has focus - hide it
-                window.orderOut(nil)
-            } else {
-                // Show and activate the window
-                NSApp.activate(ignoringOtherApps: true)
-                windowController.showWindow(nil)
-                window.makeKeyAndOrderFront(nil)
-            }
-        }
+        NSApp.activate(ignoringOtherApps: true)
+        windowController.showWindow(nil)
+        windowController.window?.makeKeyAndOrderFront(nil)
     }
 
-    private func setupMenu() {
-        let mainMenu = NSMenu()
-
-        // App Menu
-        let appMenuItem = NSMenuItem()
-        let appMenu = NSMenu()
-
-        // About menu item
-        let aboutItem = NSMenuItem(title: "About teaBASEv2", action: #selector(showAbout), keyEquivalent: "")
-        aboutItem.target = self
-        appMenu.addItem(aboutItem)
-
-        // Preferences menu item
-        let preferencesItem = NSMenuItem(title: "Preferences…", action: #selector(openSettings), keyEquivalent: ",")
-        preferencesItem.target = self
-        appMenu.addItem(preferencesItem)
-
-        appMenu.addItem(NSMenuItem.separator())
-
-        // Hide menu item
-        let hideItem = NSMenuItem(title: "Hide teaBASEv2", action: #selector(NSApplication.hide(_:)), keyEquivalent: "h")
-        appMenu.addItem(hideItem)
-
-        // Hide Others menu item
-        let hideOthersItem = NSMenuItem(title: "Hide Others", action: #selector(NSApplication.hideOtherApplications(_:)), keyEquivalent: "h")
-        hideOthersItem.keyEquivalentModifierMask = [.command, .option]
-        appMenu.addItem(hideOthersItem)
-
-        // Show All menu item
-        let showAllItem = NSMenuItem(title: "Show All", action: #selector(NSApplication.unhideAllApplications(_:)), keyEquivalent: "")
-        appMenu.addItem(showAllItem)
-
-        appMenu.addItem(NSMenuItem.separator())
-
-        // Quit menu item
-        let quitItem = NSMenuItem(title: "Quit teaBASEv2", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q")
-        appMenu.addItem(quitItem)
-
-        appMenuItem.submenu = appMenu
-        mainMenu.addItem(appMenuItem)
-
-        // Edit Menu
-        let editMenuItem = NSMenuItem()
-        let editMenu = NSMenu(title: "Edit")
-
-        let undoItem = NSMenuItem(title: "Undo", action: Selector(("undo:")), keyEquivalent: "z")
-        editMenu.addItem(undoItem)
-
-        let redoItem = NSMenuItem(title: "Redo", action: Selector(("redo:")), keyEquivalent: "Z")
-        editMenu.addItem(redoItem)
-
-        editMenu.addItem(NSMenuItem.separator())
-
-        let cutItem = NSMenuItem(title: "Cut", action: #selector(NSText.cut(_:)), keyEquivalent: "x")
-        editMenu.addItem(cutItem)
-
-        let copyItem = NSMenuItem(title: "Copy", action: #selector(NSText.copy(_:)), keyEquivalent: "c")
-        editMenu.addItem(copyItem)
-
-        let pasteItem = NSMenuItem(title: "Paste", action: #selector(NSText.paste(_:)), keyEquivalent: "v")
-        editMenu.addItem(pasteItem)
-
-        let selectAllItem = NSMenuItem(title: "Select All", action: #selector(NSText.selectAll(_:)), keyEquivalent: "a")
-        editMenu.addItem(selectAllItem)
-
-        editMenuItem.submenu = editMenu
-        mainMenu.addItem(editMenuItem)
-
-        // Window Menu
-        let windowMenuItem = NSMenuItem()
-        let windowMenu = NSMenu(title: "Window")
-
-        let minimizeItem = NSMenuItem(title: "Minimize", action: #selector(NSWindow.miniaturize(_:)), keyEquivalent: "m")
-        windowMenu.addItem(minimizeItem)
-
-        let closeItem = NSMenuItem(title: "Close", action: #selector(NSWindow.performClose(_:)), keyEquivalent: "w")
-        windowMenu.addItem(closeItem)
-
-        windowMenuItem.submenu = windowMenu
-        mainMenu.addItem(windowMenuItem)
-
-        NSApp.mainMenu = mainMenu
-    }
-
-    @objc private func showAbout() {
-        NSApp.orderFrontStandardAboutPanel(nil)
+    private func hideMainWindow() {
+        windowController?.window?.orderOut(nil)
     }
 
     @objc private func openSettings() {
@@ -146,8 +99,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
         if !flag {
-            // No visible windows, so show our window
-            windowController?.showWindow(nil)
+            showMainWindow()
         }
         return true
     }
